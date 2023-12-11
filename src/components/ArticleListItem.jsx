182b0a14c6181c1bcffeeb9bccb2ed8db62c8dca
Mike <mike@mike-francis.org>
2023-12-11 13:25:34 +0000

    added a little styling to help with development

--- src/components/ArticleListItem.jsx
+++ src/components/ArticleListItem.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 
 const ArticleListItem = ({ article }) => {
-  const { article_img_url, title, body } = article;
+  const { article_id, article_img_url, author, title, body } = article;
 
   return (
     <li className='border-gray-300 border-2 p-2'>
-      <Link to=''>
-        <img src={article_img_url} alt='' className='pb-2' />
+      <Link to={``} className="flex flex-col md:flex-row">
+        <img src={article_img_url} alt='' className='pb-2 md:w-1/2 md:pr-3' />
         <div>
           <h3 className='text-2xl pb-2'>{title}</h3>
+          <h4 className='text-xs pb-2'>By: {author}</h4>
           {/* <p className="">{body.length}</p> */}
           <p className='line-clamp-3'>{body}</p>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Main = ({ children }) => {
-  return <main className='relative top-[58px] pt-2'>{children}</main>;
+  return <main className='relative top-[58px] pt-2 md:max-w-2xl md:self-center'>{children}</main>;
 };
 
 export default Main;
+
+//  className='md:self-center max-w-2xl'
