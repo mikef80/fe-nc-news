4504f1d9d8d1e7ddfccca3029372c4b4d84b7d67
Mike <mike@mike-francis.org>
2023-12-12 10:46:16 +0000

    step 6 completed - comments added to articles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Main = ({ children }) => {
   return (
-    <main className='relative top-[58px] md:max-w-2xl md:self-center'>
+    <main className='relative top-[58px] md:max-w-3xl md:self-center pb-[42px]'>
       {children}
     </main>
   );
