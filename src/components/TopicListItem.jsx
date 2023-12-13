2fca4f044a9b42a222e4d8857da230d93255add5
Mike <mike@mike-francis.org>
2023-12-13 12:37:46 +0000

    pausing for lunch- topic icons rendering, just need to link next

--- src/components/TopicListItem.jsx
+++ src/components/TopicListItem.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getAllArticles } from "../api/api";
+import Loading from "./Loading";
 
 const TopicListItem = ({ topic }) => {
   const { slug, description } = topic;
@@ -9,31 +10,39 @@ const TopicListItem = ({ topic }) => {
     getAllArticles(slug).then(({ articles }) => setArticles(articles));
   }, []);
 
-  return (
-    <li className='flex w-[204px] flex-wrap m-4 justify-center border-2 border-black rounded-xl relative'>
-      <img
-        src={articles[0].article_img_url}
-        alt=''
-        className='rounded-tl-xl w-[100px] aspect-square object-cover'
-      />
-      <img
-        src={articles[1].article_img_url}
-        alt=''
-        className='rounded-tr-xl w-[100px] aspect-square object-cover'
-      />
-      <img
-        src={articles[2].article_img_url}
-        alt=''
-        className='rounded-bl-xl w-[100px] aspect-square object-cover'
-      />
-      <img
-        src={articles[3].article_img_url}
-        alt=''
-        className='rounded-br-xl w-[100px] aspect-square object-cover'
-      />
-      <div className='absolute top-1/2 -translate-y-1/2  text-3xl bg-white rounded-3xl p-2 border-2 border-black'>{slug}</div>
-    </li>
-  );
+  if (!articles.length) {
+    <Loading />;
+  }
+
+  if (articles.length) {
+    return (
+      <li className='flex w-[204px] flex-wrap m-4 justify-center border-2 border-black rounded-xl relative'>
+        <img
+          src={articles[0].article_img_url}
+          alt=''
+          className='rounded-tl-xl w-[100px] aspect-square object-cover'
+        />
+        <img
+          src={articles[1].article_img_url}
+          alt=''
+          className='rounded-tr-xl w-[100px] aspect-square object-cover'
+        />
+        <img
+          src={articles[2].article_img_url}
+          alt=''
+          className='rounded-bl-xl w-[100px] aspect-square object-cover'
+        />
+        <img
+          src={articles[3].article_img_url}
+          alt=''
+          className='rounded-br-xl w-[100px] aspect-square object-cover'
+        />
+        <div className='absolute top-1/2 -translate-y-1/2  text-3xl bg-white rounded-3xl p-2 border-2 border-black'>
+          {slug}
+        </div>
+      </li>
+    );
+  }
 };
 
 export default TopicListItem;
