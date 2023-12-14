e20eefcbc561b5287aa8551d1d9d7652121eb495
Mike <mike@mike-francis.org>
2023-12-14 08:40:10 +0000

    small tweaks to layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ const Header = () => {
   return (
     <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen bg-white z-10 flex justify-center font-bold'>
       <Link to='/'>
-        {/* <h1>NC News</h1> */}
         <img src={NCNewsLogo} alt="" />
       </Link>
     </header>
