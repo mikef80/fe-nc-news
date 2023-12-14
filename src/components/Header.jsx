a993c2d04aa4b60c094d82627f71475e83141c62
Mike <mike@mike-francis.org>
2023-12-14 08:48:34 +0000

    small tweaks to layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NCNewsLogo from '../img/NCNewsLogo.png'
 
 const Header = () => {
   return (
-    <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen bg-white z-10 flex justify-center font-bold py-2 md:py-0'>
+    <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen bg-white z-10 flex justify-center font-bold py-2 md:py-0 bg-[#efefef]'>
       <Link to='/'>
         <img src={NCNewsLogo} alt="" />
       </Link>
