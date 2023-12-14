a3d2d02e3791a9dc0f7884f64f033db8bfd3a19e
Mike <mike@mike-francis.org>
2023-12-14 15:07:21 +0000

    made updates to various styling elements based on feedback from Wave and aXe Devtools

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,11 @@ import NCNewsLogo from '/img/NCNewsLogo.png'
 const Header = () => {
   return (
     <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen z-10 flex justify-center font-bold py-2 md:py-0 bg-[#efefef]'>
-      <Link to='/'>
-        <img src={NCNewsLogo} alt="" />
-      </Link>
+      <h1>
+        <Link to='/'>
+          <img src={NCNewsLogo} alt="NCNews Logo. NC is written in purple with a light background, whilst News is the opposite of this." />
+        </Link>
+      </h1>
     </header>
   );
 };
--- src/components/TopicListItem.jsx
+++ src/components/TopicListItem.jsx
@@ -17,33 +17,35 @@ const TopicListItem = ({ topic }) => {
 
   if (articles.length) {
     return (
-      <Link to={`/articles?topic=${slug}`}>
-        <li className='flex w-[204px] flex-wrap m-4 justify-center border-2 border-black rounded-xl relative'>
-          <img
-            src={articles[0].article_img_url}
-            alt=''
-            className='rounded-tl-xl w-[100px] aspect-square object-cover'
-          />
-          <img
-            src={articles[1].article_img_url}
-            alt=''
-            className='rounded-tr-xl w-[100px] aspect-square object-cover'
-          />
-          <img
-            src={articles[2].article_img_url}
-            alt=''
-            className='rounded-bl-xl w-[100px] aspect-square object-cover'
-          />
-          <img
-            src={articles[3].article_img_url}
-            alt=''
-            className='rounded-br-xl w-[100px] aspect-square object-cover'
-          />
-          <div className='absolute top-1/2 -translate-y-1/2  text-3xl bg-white rounded-3xl p-2 border-2 border-black'>
-            {slug}
+      <li>
+        <Link to={`/articles?topic=${slug}`} >
+          <div className='flex w-[204px] flex-wrap m-4 justify-center border-2 border-black rounded-xl relative'>
+            <img
+              src={articles[0].article_img_url}
+              alt=''
+              className='rounded-tl-xl w-[100px] aspect-square object-cover'
+            />
+            <img
+              src={articles[1].article_img_url}
+              alt=''
+              className='rounded-tr-xl w-[100px] aspect-square object-cover'
+            />
+            <img
+              src={articles[2].article_img_url}
+              alt=''
+              className='rounded-bl-xl w-[100px] aspect-square object-cover'
+            />
+            <img
+              src={articles[3].article_img_url}
+              alt=''
+              className='rounded-br-xl w-[100px] aspect-square object-cover'
+            />
+            <div className='absolute top-1/2 -translate-y-1/2  text-3xl bg-white rounded-3xl p-2 border-2 border-black'>
+              {slug}
+            </div>
           </div>
-        </li>
-      </Link>
+        </Link>
+      </li>
     );
   }
 };
