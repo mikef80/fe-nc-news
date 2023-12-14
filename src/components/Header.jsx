505ab12c5ba9f5e476d40616d9e5db1ab5475ee1
Mike <mike@mike-francis.org>
2023-12-14 16:58:44 +0000

    styles updates to skip-to-navbar link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen z-10 flex justify-center font-bold py-2 md:py-0 bg-[#efefef]'>
       <a
         id='skip-to-content-link'
-        className='text-2xl bg-white border-2 border-black  w-full h-full md:w-[446px] md:h-[63px] flex justify-center items-center rounded-xl p-2 absolute -translate-y-full duration-300 focus:translate-y-0'
+        className='text-2xl bg-white border-2 border-black  w-full h-full md:w-[446px] md:h-[63px] flex justify-center items-center rounded-xl p-2 absolute -translate-y-[250%] duration-300 focus:translate-y-0'
         href='#navbar'>
         Skip to Navigation
       </a>
