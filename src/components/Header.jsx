b05369b558a93983c648353c0b2b71ed050ba24c
Mike <mike@mike-francis.org>
2023-12-14 16:34:26 +0000

    added a11y updates - skip to navbar button for keyboard navigators

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
-import NCNewsLogo from '/img/NCNewsLogo.png'
+import NCNewsLogo from "/img/NCNewsLogo.png";
 
 const Header = () => {
   return (
     <header className='text-center text-4xl  border-b-2 border-black fixed top-0 w-screen z-10 flex justify-center font-bold py-2 md:py-0 bg-[#efefef]'>
+      <a
+        className='text-2xl bg-white border-2 border-black  w-full h-full md:w-[446px] md:h-[63px] flex justify-center items-center rounded-xl p-2 absolute -translate-y-full duration-300 focus:translate-y-0'
+        href='#navbar'>
+        Skip to Navigation
+      </a>
       <h1>
         <Link to='/'>
-          <img src={NCNewsLogo} alt="NCNews Logo. NC is written in purple with a light background, whilst News is the opposite of this." />
+          <img
+            src={NCNewsLogo}
+            alt='NCNews Logo. NC is written in purple with a light background, whilst News is the opposite of this.'
+          />
         </Link>
       </h1>
     </header>
@@ -14,3 +22,5 @@ const Header = () => {
 };
 
 export default Header;
+
+// -translate-y-[250%]
